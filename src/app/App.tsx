f4ae85775866db82d97cc52df471e8319bff7c1f
AlyEl-deen <alyeldeenayoub@gmail.com>
2026-01-30 22:59:10 +0200

    fixing pages error

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/landing-page';
 import SubscriberForm from './components/subscriber-form';
 import RegistrationForm from './components/registration-form';
 
 export default function App() {
   return (
-    <BrowserRouter basename="/Dentalist-max-web">
+    <Router basename="/Dentalist-max-web">
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/subscribe" element={<SubscriberForm />} />
         <Route path="/register" element={<RegistrationForm />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
   
 }
