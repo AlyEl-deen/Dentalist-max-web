04ee89e3c2c2b0d8b4736b0784c44325d97f37c9
AlyEl-deen <alyeldeenayoub@gmail.com>
2026-01-31 04:57:12 +0200

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-export default({
+export default defineConfig({
   base: '/Dentalist-max-web/', 
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
